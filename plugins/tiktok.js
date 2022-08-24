9fc042126f6aedc47e350bdf12da1812e4a3f1df
YogzFX <110335811+YogzFX@users.noreply.github.com>
2022-08-25 02:11:08 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,6 +1,6 @@
 const { tiktokdl } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://vt.tiktok.com/ZSRA9hPBb/`
     const { author: { nickname }, video, description } = await tiktokdl(args[0])
     const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
     if (!url) throw 'Can\'t download video!'
